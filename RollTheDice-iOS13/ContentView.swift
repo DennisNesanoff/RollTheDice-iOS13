56203feae288676f510060ad72ade2b7c967b237
Dennis <nndenninn@gmail.com>
2020-03-25 21:58:53 +0300

    Start project

--- RollTheDice-iOS13/ContentView.swift
+++ RollTheDice-iOS13/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("GreenBackground").resizable().edgesIgnoringSafeArea(.all)
+            Text("Hello, World!")
+        }
     }
 }
 
