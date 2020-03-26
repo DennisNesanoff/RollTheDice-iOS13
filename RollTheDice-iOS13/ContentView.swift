0ec72f045fc8e73f0ddea319d5195ed99334b13e
Dennis <nndenninn@gmail.com>
2020-03-26 13:21:05 +0300

    Added functionality to the application. Final version.

--- RollTheDice-iOS13/ContentView.swift
+++ RollTheDice-iOS13/ContentView.swift
@@ -9,11 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 2
+    
     var body: some View {
         ZStack {
-            Image("GreenBackground").resizable().edgesIgnoringSafeArea(.all)
-            Text("Hello, World!")
+            Image("GreenBackground")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("joker")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer()
+                HStack {
+                    DiceView(numberString: leftDiceNumber)
+                    DiceView(numberString: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 100.0, height: 100.0)
+                }
+                .background(Color(red: 0.6060078144, green: 0.1118048355, blue: 0.1191012636))
+            }
         }
+    }
+}
+
+struct DiceView: View {
+    let numberString: Int
+    var body: some View {
+        Image("Dice\(numberString)")
+            .aspectRatio(1, contentMode: .fit)
+        .padding()
     }
 }
 
@@ -22,3 +61,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
